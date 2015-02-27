a9ae19baff00cd6d7d0364024d37f2d8f485b5a5
ambrishu <ambrishu@gmail.com>
2015-02-27 11:44:19 +0530

    Change in App.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views/'));
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 8080);
+app.set('env',  process.env.NODE_ENV || 'development');
 
 //app.use(favicon());
 //app.use(logger('dev'));
